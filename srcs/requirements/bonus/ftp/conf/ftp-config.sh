7a06d02295be86cf30bb8b6a66b7e9eeebfcc1b7
mrochedy <106345479+mrochedy@users.noreply.github.com>
2024-10-09 16:41:16 +0200

    Fix bonus FTP service

--- srcs/requirements/bonus/ftp/conf/ftp-config.sh
+++ srcs/requirements/bonus/ftp/conf/ftp-config.sh
@@ -6,6 +6,6 @@ adduser "$FTP_USER" --disabled-password
 
 echo "$FTP_USER":"$FTP_PASSWD" | /usr/sbin/chpasswd
 
-chown -R "$FTP_USER":"$FTP_USER" /var/www/web
+chown -R "$FTP_USER" /var/www/web/
 
 vsftpd /etc/vsftpd.conf
